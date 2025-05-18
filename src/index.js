2c2b5b04013e5e6d279096861ce88009c11c1fec
vaibhavpandya6 <vaibhavpandya0619@gmail.com>
2025-05-18 20:41:54 +0530

    change in utils code discussion

--- src/index.js
+++ src/index.js
@@ -2,13 +2,48 @@ import mongoose from "mongoose";
 /*require('dotenv').config({path: './env'})*/
 import dotenv from "dotenv"
 
-import connectDB from "../src/db/index.js"
+import connectDB from "../src/db/index.js" 
+import { app } from "./app.js";
 
 dotenv.config({
      path: './env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`@  Server is runnig at a port : ${process.env.PORT}`);
+    })
+
+})
+.catch((err) =>{
+    console.log("MONGO db connection failed !!! ", err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 1st method to connect database
